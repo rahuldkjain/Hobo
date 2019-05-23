a5be2d825b4009043eab75455041ce560b271eae
Mehak Puri <mehakpuri2@gmail.com>
2019-05-23 23:08:44 +0530

    frontend

--- Frontend/hobo/src/components/NavbarMerchant.vue
+++ Frontend/hobo/src/components/NavbarMerchant.vue
@@ -7,31 +7,17 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-          <!-- <router-link to="/">Home</router-link> -->
-          <!-- <router-view/> -->
-        <!-- <b-nav-item href="#">Link</b-nav-item> -->
-
-        <b-link href="/category" class="category">
-            <b-nav-item-dropdown text="All Categories" class="dropdown" center fluid>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
-        </b-link>
-        <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
+          
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" v-model="searchValue" placeholder="Search" @input="myFunc()"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
+        
         
       </b-navbar-nav>
       <b-navbar-nav>
-        <LoggedIn/>
+        <b-nav-item class="login"><router-link to="/addproduct">Add Product</router-link></b-nav-item>
+        <b-nav-item class="login" href="/">Logout</b-nav-item>
         
       </b-navbar-nav>
     </b-collapse>
@@ -42,7 +28,7 @@
 import { METHODS } from 'http';
 import LoggedIn from '@/components/LoggedIn.vue';
 export default {
-    name: 'Navbar',
+    name: 'NavbarMerchant',
     data() {
         return {
             searchValue: ''
@@ -69,7 +55,19 @@ export default {
 /* .login {
     padding-left:100px;
 } */
-
+@media screen and (max-width: 2000px) {
+    .login{
+        font-size: 25px;
+        
+        padding-left:5%;
+    }
+}
+@media screen and (max-width: 700px) {
+    .login{
+       
+        padding-left:0;
+    }
+}
 
 </style>
 
