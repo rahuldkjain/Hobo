02cb708bca5b47ccd1e1b8fcec76f40c67f9a00b
vishalpant <vshlpnt121@gmail.com>
2019-05-24 21:11:10 +0530

    Edited part 2

--- Android/app/src/main/java/com/example/hoboandroid/activities/SplashActivity.java
+++ Android/app/src/main/java/com/example/hoboandroid/activities/SplashActivity.java
@@ -1,4 +1,32 @@
 package com.example.hoboandroid.activities;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 
-public class SplashActivity {
+import com.example.hoboandroid.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start your app main activity
+                Log.d("ABCD","Enteredrun successfully");
+                Intent i = new Intent(SplashActivity.this, LandingPageActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
 }
