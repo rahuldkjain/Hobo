3a61819f0e946c648f52be12606660456d0e951c
Rahul Jain <rahuldkjain@gmail.com>
2019-05-25 13:30:46 +0530

    login initiated

--- Frontend/hobo/src/store/module.js
+++ Frontend/hobo/src/store/module.js
@@ -1,28 +1,39 @@
+import userApis from '../apis/userApis'
+
 export default {
     state: {
         loggedin: false,
-        merchant: false,
+        user: {},
+        merchant: {}
+
 
     },
     getters: {
         getLoggedIn: (state) => state.loggedin,
-        getMerchant: (state) => state.merchant
+        getMerchant: (state) => state.merchant,
+        getUser: (state) => state.user
 
     },
     mutations: {
-        SET_LOGIN: (state) => {
-            state.loggedin = !state.loggedin
+        SET_LOGIN: (state, result) => {
+            state.loggedin = true
+            state.user = result
         },
-        MERCHANT_LOGIN: (state) => {
-            state.merchantLogin = true
+        MERCHANT_LOGIN: (state, result) => {
+            state.loggedin = true
+            state.merchant = result
         }
     },
     actions: {
-        checkLogin: (context) => {
-            context.commit('SET_LOGIN')
+        checkLogin: (context, payload) => {
+            userApis.getUser((result) => {
+                context.commit('SET_LOGIN', result.data)
+            }, payload)
         },
-        merchantLogin: (context) => {
-            context.commit('MERCHANT_LOGIN')
+        merchantLogin: (context, payload) => {
+            userApis.getMerchant((result) => {
+                context.commit('MERCHANT_LOGIN', result.data)
+            }, payload)
         }
     }
 }
