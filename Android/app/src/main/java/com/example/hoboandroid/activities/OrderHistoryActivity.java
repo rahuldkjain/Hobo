d5d625059f55d97202e949df7d603e141b9beb18
amirthytejeshwar <tejuamirthi@gmail.com>
2019-05-26 16:53:31 +0530

    Buy now done

--- Android/app/src/main/java/com/example/hoboandroid/activities/OrderHistoryActivity.java
+++ Android/app/src/main/java/com/example/hoboandroid/activities/OrderHistoryActivity.java
@@ -14,6 +14,7 @@ import com.example.hoboandroid.adapters.OrderAdapter;
 import com.example.hoboandroid.models.Order;
 import com.example.hoboandroid.services.OrderService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.OkHttpClient;
@@ -27,7 +28,7 @@ public class OrderHistoryActivity extends AppCompatActivity implements View.OnCl
 
     RecyclerView orderHistoryRecyclerView;
     OrderAdapter orderAdapter;
-    List<Order> orderList;
+    List<Order> orderList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +39,10 @@ public class OrderHistoryActivity extends AppCompatActivity implements View.OnCl
 
         orderAdapter = new OrderAdapter(orderList);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(OrderHistoryActivity.this);
+     /*   LinearLayoutManager linearLayoutManager = new LinearLayoutManager(OrderHistoryActivity.this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         orderHistoryRecyclerView.setLayoutManager(linearLayoutManager);
-
+*/
 
         orderHistoryRecyclerView.setAdapter(orderAdapter);
         
