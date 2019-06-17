22b2055199caa471d57831e0dc72d8c463d8197f
Mudit Joshi <muditjoshi98@gmail.com>
2019-06-17 12:03:40 +0530

    Added logback logging

--- Backend/merchant/src/main/java/com/hobo/merchant/service/implementation/MerchantProductServiceImpl.java
+++ Backend/merchant/src/main/java/com/hobo/merchant/service/implementation/MerchantProductServiceImpl.java
@@ -10,6 +10,8 @@ import com.hobo.merchant.model.MerchantProductDTO;
 import com.hobo.merchant.repository.MerchantProductRepository;
 import com.hobo.merchant.repository.MerchantRepository;
 import com.hobo.merchant.service.MerchantProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +24,9 @@ import java.util.List;
 
 @Service
 public class MerchantProductServiceImpl  implements MerchantProductService {
+
+    private static final Logger logger
+            = LoggerFactory.getLogger(MerchantProductService.class);
     @Autowired
     private MerchantProductRepository merchantProductRepository;
 
@@ -32,11 +37,15 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
     @Transactional
     public MerchantProductDTO createMerchantProduct(MerchantProductDTO merchantProductDTO) throws MerchantProductAlreadyExists, MerchantNotFound {
 
+        logger.info(merchantProductDTO.toString());
+
         if(merchantProductRepository.exists(merchantProductDTO.getIndexx())){
+            logger.error("Data already exists error in createMerchantProduct");
             throw new MerchantProductAlreadyExists("Data already exists");
         }
 
         if (!merchantRepository.exists(merchantProductDTO.getMerchantId())){
+            logger.error("Merchant not found error in createMerchantProduct");
             throw new MerchantNotFound("Merchant not found");
         }
         MerchantProductDTO merchantProductDTO1=null;
@@ -49,28 +58,35 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
 
         merchantProductDTO1=new MerchantProductDTO();
         BeanUtils.copyProperties(merchantProduct1,merchantProductDTO1);
+        logger.info(merchantProductDTO1.toString());
         return  merchantProductDTO1;
     }
 
     @Override
     public MerchantProductDTO readMerchantProduct(Integer index) throws MerchantProductNotFound{
+        logger.info("Index : {}",index);
         if(!merchantProductRepository.exists(index)){
+            logger.error("Data not found error in readMerchantProduct");
             throw new MerchantProductNotFound("Data not found");
         }
         MerchantProduct merchantProduct= merchantProductRepository.findOne(index);
         MerchantProductDTO merchantProductDTO=new MerchantProductDTO();
         BeanUtils.copyProperties(merchantProduct,merchantProductDTO);
+        logger.info(merchantProductDTO.toString());
         return merchantProductDTO;
     }
 
     @Override
     @Transactional
     public MerchantProductDTO updateMerchantProduct(MerchantProductDTO merchantProductDTO) throws MerchantProductNotFound, MerchantNotFound{
+        logger.info(merchantProductDTO.toString());
         if(!merchantProductRepository.exists(merchantProductDTO.getIndexx()))
         {
+            logger.error("Data no found error in updateMerchantProduct");
             throw new MerchantProductNotFound("Data not found");
         }
         if (!merchantRepository.exists(merchantProductDTO.getMerchantId())){
+            logger.error("Merchant not found in updateMerchantProduct");
             throw new MerchantNotFound("Merchant not found");
         }
         MerchantProduct merchantProduct =merchantProductRepository.findOne(merchantProductDTO.getIndexx());
@@ -79,21 +95,25 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
         merchantProductRepository.save(merchantProduct1);
         //UpdateScore
         //calculateScore(merchantProduct1.getIndexx());
+        logger.info(merchantProductDTO.toString());
         return merchantProductDTO;
     }
 
     @Override
     @Transactional
     public MerchantProductDTO deleteMerchantProductById(Integer index) throws MerchantProductNotFound{
+        logger.info("Index : {}",index);
         MerchantProductDTO merchantProductDTO=readMerchantProduct(index);
         if(merchantProductDTO!=null){
             merchantProductRepository.delete(index);
         }
+        logger.info(merchantProductDTO.toString());
         return merchantProductDTO;
     }
 
     @Override
     public MerchantProductDTO getTopMerchant(Integer productId) throws MerchantProductNotFound{
+        logger.info("ProductID : {}",productId);
         MerchantProductDTO merchantProductDTO=new MerchantProductDTO();
         List<MerchantProduct> merchantProducts=merchantProductRepository.findByProductId(productId);
 
@@ -106,20 +126,22 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
         }
         if(merchantProductDTO.getIndexx()==0)
         {
+            logger.error("Data not found error in getTopMerchant");
             throw new MerchantProductNotFound("Data not found");
         }
         merchantProductDTO.setStock((int)(Math.floor(merchantProductDTO.getStock()*0.8)));
+        logger.info(merchantProductDTO.toString());
         return merchantProductDTO;
     }
 
     @Override
     public List<MerchantProductDTO> getAllMerchants(Integer productId) throws MerchantProductNotFound{
-    //public Object getAllMerchants(Integer productId) throws MerchantProductNotFound{
+        logger.info("ProductID : {}",productId);
         List<MerchantProduct> merchantProducts=merchantProductRepository.findByProductIdOrderByMerchantScoreDesc(productId);
-        //Object merchantProducts = merchantProductRepository.test(productId);
         List<MerchantProductDTO> resultArray = new ArrayList<>();
         if(merchantProducts==null)
         {
+            logger.error("Data not found error in getAllMerchants");
             throw new MerchantProductNotFound("Data not found");
         }
         for (MerchantProduct it: merchantProducts) {
@@ -128,25 +150,29 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
             BeanUtils.copyProperties(it,merchantProductDTO);
             resultArray.add(merchantProductDTO);
         }
-
+        logger.info(resultArray.toString());
         return resultArray;
     }
 
     @Override
     @Transactional
     public MerchantProductDTO updateProductRating(Integer index, float productRating) throws MerchantProductNotFound,MerchantNotFound{
+        logger.info("index : {} rating : {}",index,productRating);
         if(!merchantProductRepository.exists(index))
         {
+            logger.error("Data not found error in updateProductRating");
             throw new MerchantProductNotFound("Data not found");
         }
         MerchantProductDTO merchantProductDTO=readMerchantProduct(index);
         merchantProductDTO.setProductRating(productRating);
         MerchantProductDTO merchantProductDTO1 = updateMerchantProduct(merchantProductDTO);
+        logger.info(merchantProductDTO1.toString());
         return merchantProductDTO1;
     }
 
     @Override
     public List<MerchantProductDTO> readMerchantProductById(Integer merchantId) throws MerchantProductNotFound{
+        logger.info("MerchantId : {}",merchantId);
         List<MerchantProduct> merchantProduct=merchantProductRepository.findByMerchantId(merchantId);
         List<MerchantProductDTO> resultArray = new ArrayList<>();
         for (MerchantProduct it: merchantProduct) {
@@ -155,15 +181,19 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
             resultArray.add(merchantProductDTO);
         }
         if(merchantProduct==null){
+            logger.error("Data not found error in readMerchantProductById");
             throw new MerchantProductNotFound("Data not found");
         }
+        logger.info(resultArray.toString());
         return resultArray;
     }
 
     @Override
     public float getProductRating(Integer productId) throws MerchantProductNotFound{
+        logger.info("ProductID : {}",productId);
         List<MerchantProduct> merchantProducts=merchantProductRepository.findByProductId(productId);
         if(merchantProducts==null){
+            logger.error("Data not found error in getProductRating");
             throw new MerchantProductNotFound("Data not found");
         }
         float totalRating=0;
@@ -176,14 +206,15 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
             }
             totalRating = rating / noOfItems;
         }
+        logger.info("Rating : {}",totalRating);
         return totalRating;
     }
 
     @Override
     public MerchantProductDTO findByMerchantProductId(Integer merchantId, Integer productId) throws MerchantProductNotFound {
+        logger.info("MerchantID : {} productID : {}",merchantId,productId);
         MerchantProductDTO merchantProductDTO=new MerchantProductDTO();
         MerchantProduct merchantProduct = new MerchantProduct();
-
 
         return merchantProductDTO;
     }
@@ -191,6 +222,7 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
     @Override
     @Transactional
     public void calculateScore(int indexx) {
+        logger.info("Index : {}",indexx);
         MerchantProduct merchantProduct = merchantProductRepository.findOne(indexx);
         double updateScore;
         Integer productSold = merchantProduct.getProductsSold();
@@ -202,17 +234,21 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
 
         updateScore = 0.25*productSold+0.1*productStock+0.2*merchantRating+0.2*productRating-0.25*price;
         merchantProduct.setMerchantScore(updateScore);
+        logger.info("Score : {}",updateScore);
         merchantProductRepository.save(merchantProduct);
     }
 
     @Override
     public String checkQuantity(int id, int qty, int prodid) {
+        logger.info("Id : {} qty : {} prodid : {}",id,qty,prodid);
         MerchantProduct merchantProduct = merchantProductRepository.findByProductIdAndMerchantId(prodid,id);
         if(qty > Math.floor(merchantProduct.getStock()*0.8)) {
+            logger.info("Quantity not satisfied");
             return null;
         }
         else {
             Merchant merchant = merchantRepository.findOne(id);
+            logger.info(merchant.toString());
             return merchant.getMerchantName();
         }
     }
@@ -220,8 +256,10 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
     @Override
     @Transactional
     public String confirmQuantity(int id, int qty, int prodid) {
+        logger.info("Id : {} qty : {} prodid : {}",id,qty,prodid);
         MerchantProduct merchantProduct = merchantProductRepository.findByProductIdAndMerchantId(prodid,id);
         if(qty > (merchantProduct.getStock())) {
+            logger.info("Quantity not available");
             return null;
         }
         else {
@@ -229,6 +267,7 @@ public class MerchantProductServiceImpl  implements MerchantProductService {
             merchantProduct.setStock(merchantProduct.getStock()-qty);
             merchantProduct = merchantProductRepository.save(merchantProduct);
             Merchant merchant = merchantRepository.findOne(id);
+            logger.info(merchant.toString());
             return merchant.getMerchantName();
         }
     }
--- Backend/merchant/src/main/java/com/hobo/merchant/service/implementation/MerchantServiceImpl.java
+++ Backend/merchant/src/main/java/com/hobo/merchant/service/implementation/MerchantServiceImpl.java
@@ -9,12 +9,17 @@ import com.hobo.merchant.model.NameAndStockDTO;
 import com.hobo.merchant.repository.MerchantProductRepository;
 import com.hobo.merchant.repository.MerchantRepository;
 import com.hobo.merchant.service.MerchantService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public  class MerchantServiceImpl implements MerchantService {
+
+    private static final Logger logger
+            = LoggerFactory.getLogger(MerchantService.class);
 
     @Autowired
     private MerchantRepository merchantRepository;
@@ -23,7 +28,9 @@ public  class MerchantServiceImpl implements MerchantService {
 
     @Override
     public MerchantDTO createMerchant(MerchantDTO merchantDTO) throws MerchantAlreadyExists {
+        logger.info(merchantDTO.toString());
         if(merchantRepository.exists(merchantDTO.getMerchantId())){
+            logger.error("Data already exists error in createMerchant");
             throw new MerchantAlreadyExists("Data already exists");
         }
 
@@ -34,28 +41,32 @@ public  class MerchantServiceImpl implements MerchantService {
 
         MerchantDTO merchantDTO1=new MerchantDTO();
         BeanUtils.copyProperties(merchant1,merchantDTO1);
+        logger.info(merchantDTO1.toString());
         return merchantDTO1;
     }
 
     @Override
     public MerchantDTO readMerchantById(Integer merchantId) throws MerchantNotFound {
+        logger.info("MerchantId : {}",merchantId);
         if(!merchantRepository.exists(merchantId)){
+            logger.error("Data not found error in readMerchantId");
             throw new MerchantNotFound("Data not found");
         }
         Merchant merchant=merchantRepository.findOne(merchantId);
         MerchantDTO merchantDTO=new MerchantDTO();
         BeanUtils.copyProperties(merchant,merchantDTO);
-
+        logger.info(merchantDTO.toString());
         return merchantDTO;
     }
 
     @Override
     public MerchantDTO updateMerchant(MerchantDTO merchantDTO) throws MerchantNotFound {
-
+        logger.info(merchantDTO.toString());
         MerchantDTO merchantDTO1=null;
 
         if(!merchantRepository.exists(merchantDTO.getMerchantId()))
         {
+            logger.error("Data not found error in updateMerchant");
             throw new MerchantNotFound("Data not found");
         }
 
@@ -65,47 +76,58 @@ public  class MerchantServiceImpl implements MerchantService {
 
         merchantDTO1=new MerchantDTO();
         BeanUtils.copyProperties(merchant1,merchantDTO1);
+        logger.info(merchantDTO1.toString());
         return merchantDTO1;
     }
 
     @Override
     public MerchantDTO deleteMerchantById(Integer merchantId) throws MerchantNotFound {
+        logger.info("merchantId : {}",merchantId);
         MerchantDTO merchantDTO=readMerchantById(merchantId);
         if(merchantDTO!=null){
             Merchant merchant=new Merchant();
             BeanUtils.copyProperties(merchantDTO,merchant);
             merchantRepository.delete(merchant);
         }
+        logger.info(merchantDTO.toString());
         return merchantDTO;
     }
 
     @Override
     public MerchantDTO updateMerchantRating(Integer merchantId, float merchantRating) throws MerchantNotFound {
+        logger.info("MerchantId : {} merchantRating : {}",merchantId,merchantRating);
         Merchant merchant = merchantRepository.findOne(merchantId);
         if(merchant == null)
         {
+            logger.error("Data not found error in updateMerchantRating");
             throw new MerchantNotFound("Data not found");
         }
         merchant.setMerchantRating(merchantRating);
         merchant = merchantRepository.save(merchant);
         MerchantDTO result = new MerchantDTO();
         BeanUtils.copyProperties(merchant,result);
+        logger.info(result.toString());
         return result;
     }
 
     public String getName(Integer merchantId) throws MerchantNotFound {
+        logger.info("MerchantId : {}",merchantId);
         Merchant merchant = merchantRepository.findOne(merchantId);
         if(merchant == null) {
+            logger.error("Data not found error in getName");
             throw new MerchantNotFound("Data not found");
         }
         String result = merchant.getMerchantName();
+        logger.info(result);
         return result;
     }
 
 
     public NameAndStockDTO getNameAndStock(Integer merchantId, Integer productId) throws MerchantNotFound {
+        logger.info("MerchantId : {} productId : {}",merchantId,productId);
         Merchant merchant = merchantRepository.findOne(merchantId);
         if(merchant == null) {
+            logger.error("Data not found error in getNameAndStock");
             throw new MerchantNotFound("Data not found");
         }
         MerchantProduct merchantProduct = merchantProductRepository.findByProductIdAndMerchantId(productId,merchantId);
@@ -114,6 +136,7 @@ public  class MerchantServiceImpl implements MerchantService {
             result.setName(merchant.getMerchantName());
         if (merchantProduct!=null)
         result.setStock(merchantProduct.getStock());
+        logger.info(result.toString());
         return result;
     }
 
